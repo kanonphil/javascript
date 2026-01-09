0a59f71dd82be013e591aa64dd1a9d9eb4e8df6d
kanonphil <xoghkdbwjd@gmail.com>
2026-01-09 15:46:48 +0900

    js

--- 05.함수2.js
+++ 05.함수2.js
@@ -9,6 +9,22 @@ const hello2 = function() {
 }
 
 //화살표 함수 사용
-const hello3 = () => {
-  console.log("hello3");
+const hello3 = () => console.log("hello3");
+//화살표 함수에서 매개변수가 하나일 때는 괄호 생략 가능
+
+function intro1() {
+  console.log("반가워");
+  console.log("또 만나");
+}
+
+//함수 표현식
+const intro2 = function() {
+  console.log("반가워");
+  console.log("또 만나");
+}
+
+//화살표 함수
+const intro3 = () => {
+  console.log("반가워");
+  console.log("또 만나");
 }
